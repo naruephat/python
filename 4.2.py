25e8d90685c4c4dd0921cd747bb1177152fe1d4a
naruephat <77142376+naruephat@users.noreply.github.com>
2021-02-11 01:13:20 +0700

    Update 4.2.py

--- 4.2.py
+++ 4.2.py
@@ -1,27 +1,41 @@
-print('\t          พจณานุกรม')
-print('*'*50)
-wocab=[]
-wocab1=[]
-wocab2=[]
-num1=[]
-def menu():
-        global coish
-        print('[1]เพิ่มคำศัพท์\n','[2]เพิ่มคำศัพท์\n','[3]เพิ่มคำศัพท์\n','[4]เพิ่มคำศัพท์')
-        coish=int(input(' '))
-        print('-'*30)
-def up():
-    print('ตัวเลือกที่1')
-    sub=str(input('เพิ่มคำศัพท์\n'))
-    wocab.append(sub)
-    sub1=str(input('ชนิดคำศัพท์(n.,v.,adj.,adv.,'))
-    if sub1 == 'n':
-        num1.append(0)
-    if sub1 == 'v':
-        num1.append(1)
-    if sub1 == 'adj':
-        num1.append(2)
-    if sub1 == 'adv':
-        num1.append(3)
-sub2=str(input('ความหมาย\t'))
-woccab2.append(sub2)
-print('เพิ่มคำศัพท์เสร็จสมบูรณ์')
+a = 5
+vocabulary = {
+    'Gun':'       n.คำนาม   ปืน',
+    'Run':'       v.คำกริยา  วิ่ง',
+    'dog':'       n.คำนาม   หมา',
+}
+while(True):
+    print('พจนานุกรม\n 1)เพิ่มคำศัพท์\n 2)เเสดงคำศัพท์\n 3)ลบคำศัพท์\n 4)ออกจากโปรเเกรม\n')
+    data = int(input('Input choice :'))
+    if data == 1 :
+        a += 1
+        terminology = input('เพิ่มคำศัพท์\a:')
+        type_word = input('ชนิดของคำ (ชนิดของคำ(n.,v.,adj.,adv.) :')
+        if type_word == 'n.':
+            type_word = 'n.คำนาม'
+        elif type_word == 'v.':
+            type_word = 'v.คำกริยา'
+        elif type_word == 'adj.':
+            type_word = 'adj.ขยายคำนาม.'
+        elif type_word == 'adv.':
+            type_word = 'adv. กริยาวิเศษณ์'
+        meaning = input('ความหมาย\t :')
+        vocabulary[terminology] =  '   '+type_word+'   '+meaning
+        print('เพิ่มคำศัพท์เรียบร้อยเเล้ว')
+    elif data == 2 :
+        print(15*'-'+'\n คำศัพท์มีทั้งหมด ',t,' คำ\n',15*'-')
+        print('คำศัพท์           ประเภท       ความหมาย')
+        for i in vocabulary :
+            print(i+vocabulary[i])
+    elif data == 3 :
+        delete = input('พิมพ์คำศัพท์ที่ตองการลบ :')
+        b = input('ต้องการลบ'+delete+'ใช่หรือไม่ (yes / no):')
+        if b == 'y':
+            vocabulary.pop(delete)
+            a -= 1
+            print('ลบ'+delete+'เรียบร้อยเเล้ว')
+    else:
+        yes = input('ต้องการออกจากโปรเเกรมใช่หรือไม่ (yes / no):')
+        if yes == ' yes':
+            print('ออกจากโปรเเกรมเรียบร้อยเเล้ว')
+            break
